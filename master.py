9836486415bf9d3b8196028546adac340bb3ca35
Gabriel Benitez <s1752178@bioinfmsc5.bio.ed.ac.uk>
2020-11-10 21:27:13 +0000

    Made master file for running code

---
+++ master.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+
+#######
+## 1 ##  User specifies protein family and taxonomic group
+#######
+
+
+
+
+
+#######
+## 2 ##  Main processing
+#######
+
+
+
+
+
+
+
+#######
+## 3 ##  PROSITE database motif scan
+#######
+
+
+
+
+
+
+
+#######
+## 4 ##  Wildcard
+#######
