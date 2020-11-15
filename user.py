8e628ef5deebd5c23a61ad6c66c1cea732b8980d
Gabriel Benitez <s1752178@bioinfmsc5.bio.ed.ac.uk>
2020-11-15 13:37:09 +0000

    Nice little print statements for user script

--- user.py
+++ user.py
@@ -1,6 +1,27 @@
 #!/bin/python3
 
 
+#this part of the script will get the user to specify protein family and tax group
+#then it will fetch the relevant information from the user input
+
+
 #######
 ## a ## User specifies protein family and taxonimic group
 #######
+
+#first lets import the modules needed
+import os, re
+
+
+#Introductory lines
+print ("Welcome to B120271\'s  tool for protein conservation and motif ID analysis")
+
+
+#receive input for protein
+prot = input("Please input protein family of interest:")
+print(prot)
+
+
+#receive input for taxonomic group
+tax = input("Please input subset of taxonomic tree of interest:")
+print (tax)
