b0da71b76c71efb7c3ee7f3a175dec832351ea47
Gabriel Benitez <s1752178@bioinfmsc5.bio.ed.ac.uk>
2020-11-15 14:48:28 +0000

    receive user input done

--- user.py
+++ user.py
@@ -6,22 +6,45 @@
 
 
 #######
-## a ## User specifies protein family and taxonimic group
+## a ## User specifies protein family and taxonomic group
 #######
 
 #first lets import the modules needed
-import os, re
+import os, subprocess, sys, re
 
 
 #Introductory lines
-print ("Welcome to B120271\'s  tool for protein conservation and motif ID analysis")
-
-
-#receive input for protein
-prot = input("Please input protein family of interest:")
-print(prot)
+print ("Welcome to B120271\'s  tool for protein conservation and motif ID analysis",
+"\n")
 
 
 #receive input for taxonomic group
-tax = input("Please input subset of taxonomic tree of interest:")
-print (tax)
+prot = input("Please input subset of taxonomic tree of interest:\n")
+
+
+#receive input for protein of interest
+tax = input("Please input protein family of interest:\n")
+
+
+#confirm analysis
+print("Analysis will be performed on the\n", 
+prot, " protein family\n"
+, "in the \n", 
+tax, "taxonomic subset")
+
+print("\n")
+
+proceed = input("Do you wish to continue? [yes/no] \n")
+
+if proceed == "yes":
+	print ("Continuing analysis...")
+else:
+	sys.exit()
+	
+
+#we make a folder to store our sequences and navigate there
+os.mkdir("database")
+os.chdir("database")
+
+
+#lets go fetch our sequences now
