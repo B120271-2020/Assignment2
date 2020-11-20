a9e7d9c3b61ceed1ca9c0efcc0f03caf8c844e57
Gabriel Benitez <s1752178@bioinfmsc5.bio.ed.ac.uk>
2020-11-20 18:55:46 +0000

    updated process with workflow

---
+++ process.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+#######
+## 2 ##	Plotting conservation between protein sequences
+#######
+
+#determine and plot conservation levels between sequenecs across taxonoic groups
+#establish similarity within user sequence set chosen
+#output to screen and save as file output
+#limit number of sequences to no more than 250 most similar
+
+
+
+# we can use emboss plotcon for it
+# or clustalo too
+# or blast
+
+# we need to make a protein database with our previous data
+
