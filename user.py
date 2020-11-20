36ba38c17e5921734233501da40b196322d64b30
Gabriel Benitez <s1752178@bioinfmsc5.bio.ed.ac.uk>
2020-11-20 17:54:41 +0000

    updated user to run through a loop allowing for restarts

--- user.py
+++ user.py
@@ -1,70 +1,98 @@
 #!/bin/python3
 
-
-#this part of the script will get the user to specify protein family and tax group
-#then it will fetch the relevant information from the user input
-
-
 #######
-## a ## User specifies protein family and taxonomic group
+## a ## getting data from user defined query
 #######
+
+
 
 #first lets import the modules needed
 import os, subprocess, sys, re
+
+
+#we make a folder for our assignment and navigate there
+if not os.path.exists("database"):
+        os.mkdir("database")
+os.chdir("database")
 
 
 #Introductory lines
 print ("\nWelcome to B120271\'s  tool for protein conservation and motif ID analysis\n")
 
 
-#receive input for taxonomic group
-tax = input("Please input subset of taxonomic tree of interest:\n")
+#we establish a loop for user inputs
+
+i=1
+
+while i == 1:
+	#receive input for taxonomic group
+	tax = input("Please input subset of taxonomic tree of interest:\n")
 
 
-#receive input for protein of interest
-prot = input("Please input protein family of interest:\n")
+	#receive input for protein of interest
+	prot = input("Please input protein family of interest:\n")
 
 
-#confirm analysis
-print("\nAnalysis will be performed on the\n", 
-prot, " protein family\n"
-, "in the \n", 
-tax, "taxonomic subset\n")
+	#confirm analysis
+	print("\nAnalysis will be performed on the\n",
+	prot," protein family\n",
+	"in the\n",
+	tax, "taxonomic subset\n")
 
-proceed = input("Do you wish to continue? [yes/no] \n")
+	proceed = input("Do you wish to continue? [yes/no] \n")
 
-if proceed == "yes":
-	print ("Fetching Information...")
-else:
-	sys.exit()
-	
-
-#we make a folder to store our sequences and navigate there
-os.mkdir("database")
-os.chdir("database")
+	if proceed == "yes":
+		print ("Fetching data...")
+	else:
+		sys.exit()
 
 
-#lets go fetch our sequences now
-#we first copy our current environment to retain our variables
-current_env = os.environ.copy()
+	#lets go fetch our sequences now
+	#we first copy our current environment to retain our variables
+	current_env = os.environ.copy()
 
-#adding these to our dictionary just in case
-current_env ["wow"] = prot
-current_env ["wowe"] = tax
 
-#we output the fasta information using the esearch commands
-subprocess.call('esearch -db protein \
--query "$wowe [organism] AND $wow [protein]" |\
- efetch -format fasta > data.txt', env=current_env, shell=True)
+	#adding these to our dictionary just in case
+	current_env ["wowe"] = tax
+	current_env ["wow"] = prot
 
-#update the user on progress
-print("Data successfully downloaded...")
 
-#proceed options
+	#we output the fasta information using the esearch commands
+	subprocess.call('esearch -db protein \
+	-query "$wowe [organism] AND $wow [protein]" |\
+	efetch -format fasta > data.txt', env=current_env, shell=True)
+
+
+	#update the user on progress
+	print("Data successfully downloaded")
+
+
+	#reading through the data to let the user know what they have downloaded
+	data = open("data.txt").read()
+	print("\nWithin your defined dataset,")
+
+
+	#we count the number of sequences as >
+	seq = data.count('>')
+	print("The total number of sequences is:\n", seq)
+
+
+	#we count the number of non redunant species
+	spec = "peepee poopoo"
+	print("The total number of unique species is:\n", spec)
+
+	#if this is not acceptable we ask user if they want to restart
+	print("\nWARNING! Program will only analyse top the 250 most similar sequences")
+
+	next = input("Would you like to redefine your query? [yes/no]\n")
+
+	if next == "yes":
+		#restart the loop 
+		i = 1
+	else:
+		#exit the loop
+		i = 2 
+
+
+#Going to the next step
 print("Proceeding with conservation analysis...")
-
-#look through output files and get bare data about it to present to the reader
-#like number of sequences, etc
-#if term too narrow, prompted to search for broader parameters
-#could take the user back to the start in a loop or something
-
