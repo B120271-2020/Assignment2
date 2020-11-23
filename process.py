15ad2d2d6adebd4b04a4e1552c8b92b5c4647019
Gabriel Benitez <s1752178@bioinfmsc5.bio.ed.ac.uk>
2020-11-23 00:21:23 +0000

    process.py

--- process.py
+++ process.py
@@ -10,22 +10,47 @@ import os, subprocess, sys, re
 #this is redundant for the final script
 os.chdir("database")
 
-#we align the data using clustalo under subprocess command
-subprocess.call('clustalo -i data.txt --outfmt=fasta -v -o output.fa', shell=True)
+#we make a database from the users query
+subprocess.call('makeblastdb -in data.txt -dbtype prot -out reference', shell=True)
+print("Reference database successfully created")
 
 
-#we first 
+#use cons to get a query sequence
+cmd = 'cons data.txt'
+print("To verify you are not a robot, please input the following within the square brackets [yes]")
+subprocess.call(cmd, shell=True)
 
-#determine and plot conservation levels between sequenecs across taxonoic groups
-#establish similarity within user sequence set chosen
-#output to screen and save as file output
-#limit number of sequences to no more than 250 most similar
+#we blast our query prompt
+subprocess.call('blastp -db reference -query yes -outfmt 7 > blastoutput.out', shell = True)
+subprocess.call('blastp -db reference -query yes -outfmt "6 sseqid sseq" > blastoutput.out', shell = True)
+
+#to format into fasta
+subprocess.call("sed 's/^/>/' blastoutput.out > temp", shell = True)
+
+#trim
+subprocess.call("head -n 250 temp > temp2", shell = True)
+
+#output now fasta
+text = open("temp2").read()
+text2 = text.replace('\t', '\n')
+f = open("trim", "w")
+f.write(text2)
+f.close()
+
+#align 
+subprocess.call('clustalo -i trim --outfmt=fasta -v -o align.fa --output-order=tree-order --iter=3', shell=True)
+print("Data successfully aligned")
+
+#plot
+subprocess.call('plotcon -sformat fasta align.fa -graph svg', shell=True)
+subprocess.call('xdg-open plotcon.svg', shell=True)
 
 
 
-# we can use emboss plotcon for it
-# or clustalo too
-# or blast
+#plotcon is used to process a conservation plot from clustalo output
+print("Proceeding with convservation plot")
+print("When prompted, input size of windows. Larger windows result in smoother plots")
 
-# we need to make a protein database with our previous data
-
+#the program can now continue after the user exits
+print("Conservation plot for variables here successfuly generated")
+print("Scanning for motifs from the prosite database")
