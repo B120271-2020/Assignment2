bed7069a1031b1d5dc38fbb61f2202f0b11d64e3
Gabriel Benitez <s1752178@bioinfmsc5.bio.ed.ac.uk>
2020-11-23 09:42:04 +0000

    tidied up previous

--- process.py
+++ process.py
@@ -4,7 +4,7 @@
 ## 2 ##	Plotting conservation between protein sequences
 #######
 
-import os, subprocess, sys, re
+import os, subprocess, time
 
 #for this section of script we navigat to the databse folder
 #this is redundant for the final script
@@ -17,39 +17,43 @@ print("Reference database successfully created")
 
 #use cons to get a query sequence
 cmd = 'cons data.txt'
-print("To verify you are not a robot, please input the following within the square brackets [yes]")
+print("To verify you are not a robot, please input the following within the square brackets after the next prompt [yes]")
+
+#we wait to make sure user has read the prompt
+#this process of file naming is a bit of a cop-out
+time.sleep(3)
+
+#the user input of yes specifies the file name used in the next command
 subprocess.call(cmd, shell=True)
 
 #we blast our query prompt
-subprocess.call('blastp -db reference -query yes -outfmt 7 > blastoutput.out', shell = True)
 subprocess.call('blastp -db reference -query yes -outfmt "6 sseqid sseq" > blastoutput.out', shell = True)
 
-#to format into fasta
+#we format the blast into a "semi-fasta"
 subprocess.call("sed 's/^/>/' blastoutput.out > temp", shell = True)
 
-#trim
+#trim for the top 250 represented sequences
 subprocess.call("head -n 250 temp > temp2", shell = True)
 
-#output now fasta
+#replacing each tab with a newline to get a fasta format
 text = open("temp2").read()
 text2 = text.replace('\t', '\n')
 f = open("trim", "w")
 f.write(text2)
 f.close()
 
-#align 
+#align with our file
 subprocess.call('clustalo -i trim --outfmt=fasta -v -o align.fa --output-order=tree-order --iter=3', shell=True)
 print("Data successfully aligned")
-
-#plot
-subprocess.call('plotcon -sformat fasta align.fa -graph svg', shell=True)
-subprocess.call('xdg-open plotcon.svg', shell=True)
-
 
 
 #plotcon is used to process a conservation plot from clustalo output
 print("Proceeding with convservation plot")
-print("When prompted, input size of windows. Larger windows result in smoother plots")
+print("When prompted, input size of windows. Larger windows result in smoother plots, at loss of sensitivity")
+
+#we plot using our aligned data
+subprocess.call('plotcon -sformat fasta align.fa -graph svg', shell=True)
+subprocess.call('xdg-open plotcon.svg', shell=True)
 
 #the program can now continue after the user exits
 print("Conservation plot for variables here successfuly generated")
--- user.py
+++ user.py
@@ -52,13 +52,13 @@ while i == 1:
 
 
 	#adding these to our dictionary just in case
-	current_env ["wowe"] = tax
-	current_env ["wow"] = prot
+	current_env ["tax"] = tax
+	current_env ["prot"] = prot
 
 
 	#we output the fasta information using the esearch commands
 	subprocess.call('esearch -db protein \
-	-query "$wowe [organism] AND $wow [protein]" |\
+	-query "$tax [organism] AND $prot [protein]" |\
 	efetch -format fasta > data.txt', env=current_env, shell=True)
 
 
@@ -82,7 +82,7 @@ while i == 1:
 
 	#we count the number of non redunant species
 	nspec = set(re.findall('\[(.*?)\]', data))
-	print("The total number of unique species is:\n", len(nspec))
+	print("The total number of unique species represented:\n", len(nspec))
 
 	#option to redefine query
 	next = input("\nWould you like to redefine your query? [yes/no]\n")
@@ -97,3 +97,59 @@ while i == 1:
 
 #Going to the next step
 print("Proceeding with conservation analysis...")
+
+
+#######
+## 2 ##
+#######
+
+#we make a database from the users query
+subprocess.call('makeblastdb -in data.txt -dbtype prot -out reference', shell=True)
+print("Reference database successfully created")
+
+
+#use cons to get a query sequence
+cmd = 'cons data.txt'
+print("To verify you are not a robot, please input the following within the square brackets [yes]")
+
+#we wait to make sure user has read the prompt
+#this process of file naming is a bit of a cop-out
+time.sleep(3)
+
+#the user input of yes specifies the file name used in the next command
+subprocess.call(cmd, shell=True)
+
+#we blast our query prompt
+subprocess.call('blastp -db reference -query yes -outfmt "6 sseqid sseq" > blastoutput.out', shell=True)
+
+#we format the blast into a "semi-fasta"
+subprocess.call("sed 's/^/>/' blastoutput.out > temp", shell = True)
+
+#trim for the top 250 represented sequences
+subprocess.call("head -n 250 temp > temp2", shell = True)
+
+#trim for the top 250 represented sequences
+subprocess.call("head -n 250 temp > temp2", shell = True)
+
+#replacing each tab with a newline to get a fasta format
+text = open("temp2").read()
+text2 = text.replace('\t', '\n')
+f = open("trim", "w")
+f.write(text2)
+f.close()
+
+#align with our file
+subprocess.call('clustalo -i trim --outfmt=fasta -v -o align.fa --output-order=tree-order --iter=2', shell=True)
+print("Data successfully aligned")
+
+#plotcon is used to process a conservation plot from clustalo output
+print("Proceeding with convservation plot")
+print("When prompted, input size of windows. Larger windows result in smoother plots, at loss of sensitivity")
+
+#we plot using our aligned data
+subprocess.call('plotcon -sformat fasta align.fa -graph svg', shell=True)
+subprocess.call('xdg-open plotcon.svg', shell=True)
+
+#the program can now continue after the user exits
+print("Conservation plot for variables here successfuly generated")
+print("Scanning for motifs from the prosite database")
